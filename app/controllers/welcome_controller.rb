ecb0dd7f4e0c79058a82bf9687612cb3c349ada7
Yup Tan <seventanyb@gmail.com>
2013-10-05 16:39:28 +0800

    i

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,8 +2,6 @@ class WelcomeController < ApplicationController
   layout 'special'
   def home
     Rails.logger.debug "entry home method"
-    flash.now[:notice] = "flash now test!"
-    # redirect_to root_path
     # raise 'Deliberate Failure'
     # render 'welcome/home', layout: 'special'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Iks::Application.routes.draw do
-  get "welcome/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
